bfed12cc54afd8828a19869d7c5a5b5054f6a2c0
Ridhay1 <ridhayparhar@gmail.com>
2026-09-04 12:43:19 -0600

    MadLibs 2
    This is my proper one where we learned how to code all of this and change the noun places and problem solve

---
+++ Main.p/My Main.py
@@ -0,0 +1,10 @@
+noun = input("Input a noun: ")
+verb = input("Input a verb ending in -ing: ")
+adverb = input("Input an adverb: ")
+plural_noun = input("Input a plural noun: ")
+adjective = input("Input an adjective: ")
+
+
+message = f"Today at school, my team played volleyball with a {noun}. First, we grabbed the item while {verb}, then everyone cheered {adverb}. Suddenly, someone accidentally hit {plural_noun}! Everyone said, This is the most {adjective} game I've ever seen./'"
+
+print(message)
